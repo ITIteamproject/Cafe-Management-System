e3f8e50856402c6396186c81f8c51760af157c8c
AbdallahNagy <abdallahnagy773@gmail.com>
2023-02-04 17:33:55 +0200

    make user router js file

--- api/server.js
+++ api/server.js
@@ -3,11 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const userRouter = require('./routers/usersRouter');
 
 
-
-
-
+app.use('/users', userRouter)
 
 // connect to database 
 mongoose.connect('mongodb+srv://member:member123@cluster.cmlunqp.mongodb.net/cafeDB', (err) => {
